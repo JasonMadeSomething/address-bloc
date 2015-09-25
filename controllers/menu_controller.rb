a3fa6b0865e6647e4cad850ce15773c17d9ec722
Jason Teitelman <super.soviet.jesus@gmail.com>
2015-09-25 19:19:40 +0000

    Complete assignment 25, menu2

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - Delete all records"
+    puts "6 - Exit"
     print "Enter your selection: "
     
     selection = gets.to_i
@@ -36,6 +37,9 @@ class MenuController
       read_csv
       main_menu
     when 5
+      system "clear"
+      nuke
+    when 6
       puts "Good-bye!"
       exit(0)
     else
@@ -43,6 +47,10 @@ class MenuController
       puts "Sorry, that is not a valid input"
       main_menu
     end
+  end
+  
+  def nuke
+    @address_book = AddressBook.new
   end
   
   def view_all_entries
